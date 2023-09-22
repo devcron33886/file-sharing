0f73c851da29c555dc83e12f0a27ab14ae403180
LofiCoders <71966425+devcron33886@users.noreply.github.com>
2023-09-22 12:12:26 +0200

    Worked on document branch

--- app/Http/Requests/StoreDocumentRequest.php
+++ app/Http/Requests/StoreDocumentRequest.php
@@ -6,23 +6,23 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDocumentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required,max:255,string',
+            'department_id' => 'required,integer,exists:departments,id',
+            'receiver_id' => 'required,integer,exists:users,id',
+            'documents' => 'required|mimes:pdf,doc,docx,jpg,jpeg,png|max:2048',
+            'sender_id' => 'required,integer,exists:users,id',
+            'source' => 'required',
+            'notes' => 'required',
+            'status' => 'required',
+
         ];
     }
 }
