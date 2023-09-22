1848b42f22711a8fdc4d152335a9f8ac4e09a142
LofiCoders <71966425+devcron33886@users.noreply.github.com>
2023-09-22 11:23:37 +0200

    Worked migration files of users and departments

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
 }
--- database/migrations/2023_09_21_224339_create_departments_table.php
+++ database/migrations/2023_09_21_224339_create_departments_table.php
@@ -6,22 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
             $table->timestamps();
+            $table->softDeletes();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('departments');
     }
 };
